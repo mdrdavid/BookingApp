3b8636479944bb11cc1f04b2454fd2cf71fd6f0c
david <davidmatovu88@gmail.com>
2022-06-11 20:22:15 +0300

    create more components

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import "./featuredProperties.css";
 import useFetch from "../../hooks/useFetch";
 
 const FeaturedProperties = () => {
-  const {data,loading, error}= useFetch("hotels?featured=true&limit=4");
+  const {data,loading}= useFetch("hotels?featured=true&limit=4");
 
   return (
     <div className="fp">
@@ -48,31 +48,34 @@ const FeaturedProperties = () => {
           <span>Excellent</span>
         </div>
       </div> */}
-      {loading ? "Loading":
-      <>
-       {data.map(item=>{
-        <div className="fpItem" key={item._id}>
-        <img
-        src={item.photos[0]}
-          // src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
-          alt=""
-          className="fpImg"
-        />
-        {/* <span className="fpName">Hilton Garden Inn</span> */}
-        <span className="fpName">{item.name}</span>
-        {/* <span className="fpCity">Berlin</span> */}
-        <span className="fpCity">{item.city}</span>
-        {/* <span className="fpPrice">Starting from $105</span> */}
-        <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-        {item.rating && 
-        <div className="fpRating">
-          {/* <button>8.9</button> */}
-          <button>{item.rating}</button>
-          <span>Excellent</span>
-        </div>}
-      </div>
-      })}
-      </>}
+
+      {loading ? "Loading" :
+        <>
+          {data.map((item) => {
+            <div className="fpItem" key={item._id}>
+              <img
+                src={item.photos[0]}
+                // src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
+                alt=""
+                className="fpImg"
+              />
+              {/* <span className="fpName">Hilton Garden Inn</span> */}
+              <span className="fpName">{item.name}</span>
+              {/* <span className="fpCity">Berlin</span> */}
+              <span className="fpCity">{item.city}</span>
+              {/* <span className="fpPrice">Starting from $105</span> */}
+              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+              {item.rating &&
+                <div className="fpRating">
+                  {/* <button>8.9</button> */}
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>}
+            </div>
+          })}
+        </>
+      }
+
     </div>
   );
 };
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -15,8 +15,13 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const {data,loading, error}= useFetch(`/hotel?city${destination}`)
+  const {data,loading, error,reFetch}= useFetch(`/hotels?city${destination}&min=${min ||0}&max=${max|| 999}`)
+  const handleClick=()=>{
+    reFetch()
+  }
   return (
     <div>
       <Navbar />
@@ -50,13 +55,13 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e=>setMin(e.target.validity)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e=>setMax(e.target.validity)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -87,7 +92,7 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
             {/* <SearchItem />
@@ -101,7 +106,7 @@ const List = () => {
             {loading ? "Loading" :
             <>
             {faCloudDownloadAlt.map(item=>{
-              <SearchItem item ={itme} key={item._id}/>
+              <SearchItem item ={item} key={item._id}/>
             })}
             
             </>}
